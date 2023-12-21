ea2e885f4aab041f56ec7a360648cf6a87741ad8
pjgowtham <pjgowtham@gmail.com>
2023-12-21 15:42:43 +0530

    Build oplus camera if available

--- product.mk
+++ product.mk
@@ -10,6 +10,11 @@ ifneq (,$(wildcard packages/apps/Bellis))
 PRODUCT_PACKAGES += Bellis
 endif
 
+# Camera
+ifneq (,$(wildcard vendor/oplus/camera))
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+endif
+
 # Default ADB shell prompt
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash
